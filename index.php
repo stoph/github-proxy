5b7cf8aec59504d587cc50624d39ec69c3351ec8
Christoph Khouri <christoph.khouri@gmail.com>
2024-04-12 13:28:12 -0400

    Adding partial example

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Super simple proxy service I built for a very specific need that no one else besides me will probably ever use">
   <title>Github Proxy</title>
   <script src="https://cdn.tailwindcss.com/"></script>
 </head>
@@ -15,6 +16,7 @@
         Sample URL Format
       </label>
       <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="url" type="text" value="https://github-proxy.com/archive/{repo}[/{branch}]" readonly>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="url" type="text" value="https://github-proxy.com/partial/{repo}/{directory}" readonly>
     </div>
   </div>
   <footer class="fixed inset-x-0 bottom-0 py-4 bg-gray-800 text-center text-xs text-gray-300 grid grid-cols-3 gap-4">
