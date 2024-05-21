5005e2d383559082afbea48468c307cb34481e78
Christoph Khouri <christoph.khouri@gmail.com>
2024-05-20 23:20:56 -0400

    Displaying error messages with a plugin

--- index.php
+++ index.php
@@ -6,14 +6,6 @@ if ( !isset($_GET['url']) ) {
 
 $gh_url = 'https://github.com/' . $_GET['url'];
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET');
-header('Content-Type: application/zip');
-header('Content-Disposition: attachment; filename="github.zip"');
-header('Pragma: no-cache');
-readfile($gh_url);
-
-/*
 // Only download zip files.
 $ch = curl_init($gh_url);
 curl_setopt($ch, CURLOPT_NOBODY, true);
@@ -22,13 +14,60 @@ curl_setopt($ch, CURLOPT_HEADER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_exec($ch);
 
-$content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+$curl_info = curl_getinfo($ch);
 curl_close($ch);
 
-if ($content_type === 'application/octet-stream') {
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+header('Content-Type: application/zip');
+// header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="github.zip"');
+header('Pragma: no-cache');
+
+$error_array = [];
+if ($curl_info['content_type'] != 'application/octet-stream') {
+  array_push($error_array, '• Unexpected content type. Expected <i>application/octet-stream</i>, got <i>' . $curl_info['content_type'] . '</i>');
+}
+if ($curl_info['http_code'] != 200) {
+  array_push($error_array, '• Unexpected HTTP code. Expected <i>200</i>, got <i>' . $curl_info['http_code'] . '</i>');
+}
+// Not sure if this is a consistent/reliable header yet.
+// if ($curl_info['download_content_length'] <= 0) {
+//   array_push($error_array, '• Empty file');
+// }
+
+if (empty($error_array)) {
+  header('Content-Length: ' . $curl_info['download_content_length'] );
   readfile($gh_url);
 } else {
-  // Return a predefined plugin that displays the error message in the admin :)
-  die('The file is not an application/octet-stream.');
+  // Return a plugin that displays the error message in the admin :)
+  $message = '';
+  foreach($error_array as $error) {
+    $message .= $error . '<br>';
+  }
+
+  $plugin = <<<PLUGIN
+<?php
+    /*
+    Plugin Name: Error Message Plugin
+    Description: Temporary error message from Github Proxy for WP Playground
+    Author: Github Proxy
+    */
+    function displayProxyErrorMessage() {
+      echo '<div class="notice notice-error"><p>The proxied file generated an error from Github.<br>';
+      echo '$message';
+      echo '</p></div>';
+    }
+    add_action('admin_notices', 'displayProxyErrorMessage');
+    ?>
+PLUGIN;
+  
+  $zip = new ZipArchive();
+  $zip_file = 'checkouts/error_plugin.zip';
+  $ret = $zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+  $zip->addFromString('error_plugin.php', $plugin);
+  $zip->close();
+  //header('Content-Length: ' . filesize($zip_file));
+  readfile($zip_file);
+  unlink($zip_file);
 }
-*/
